f1897e587e6823579259aad4b25f8fdf2ba1ce1b
Fady Fawzy <FadyFawzy.Official@gmail.com>
2022-04-15 10:42:31 +0200

    [VIC] Run the app now & Start building the UI widgets with the Hot Reload help ;)

--- lib/counter/view/counter_view.dart
+++ lib/counter/view/counter_view.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 
-//* Note: I just build this "now" to be available to export (to be imported)
-//* into anywhere depend on the Counter View (UI).
-//! I want to be able to build the UI widgets using hot reload feature.
-
 class CounterView extends StatelessWidget {
   const CounterView({Key? key}) : super(key: key);
 
